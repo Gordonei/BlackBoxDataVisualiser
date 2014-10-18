30d6fa488dbd95edc61b80e4dbe77addca864f23
Gordon Inggs <gordon.inggs@gmail.com>
2014-10-18 13:52:21 +0100

    More comments. Added helper function to detect end of header

--- BlackBoxDataVisualiser.py
+++ BlackBoxDataVisualiser.py
@@ -3,19 +3,42 @@ import sys
 import numpy, matplotlib.pyplot as plt
 from matplotlib.widgets import RadioButtons
 
-def Read(filename,strip_whitespace=True,row_separator='\n',col_separator=','):
+def headerSnoop(raw_data,col_separator):
+    """
+    Helper function for trying to detect where the data file header ends and the data begins.
+    """
+    header_length_guess = 0
+    for i,rd in enumerate(numpy.array(raw_data)):
+        #This probably isn't a very Pythonic way to do this...
+        try:
+            numpy.array(rd.split(col_separator)[:-1]).astype(numpy.float)
+            if not(header_length_guess): header_length_guess = i
+        
+        except(ValueError): pass
+    
+    return header_length_guess
+
+
+def Read(filename,strip_whitespace=True,row_separator='\n',col_separator=',',header_rows=0,title_row=0):
+    """
+    High level function for reading in data from a flat file.
+    """
     #Reading data in from file
     datafile = open(filename,"r")
     raw_data = datafile.read().split(row_separator)
     if(strip_whitespace): raw_data = filter(None,raw_data)
     
-    #Title is first line, Headers are the second
-    title = raw_data[0].strip(col_separator)
-    headers = raw_data[1].split(col_separator)
+    #Title is first line, Column Headers are the last line of the header
+    title = raw_data[title_row].strip(col_separator)
+    
+    #If header rows aren't specified, use the snooper helper function to try 
+    if not(header_rows): header_rows = headerSnoop(raw_data,col_separator)
+    
+    headers = raw_data[header_rows-1].split(col_separator)
     
     #Turning data in numpy array
-    if(strip_whitespace): data = [tuple(filter(None,d.split(col_separator))) for d in raw_data[2:]]
-    else: data = [tuple(d.split(col_separator)) for d in raw_data[2:]]
+    if(strip_whitespace): data = [tuple(filter(None,d.split(col_separator))) for d in raw_data[header_rows:]]
+    else: data = [tuple(d.split(col_separator)) for d in raw_data[header_rows:]]
     
     #Adding the headers to the datafile
     number_formats = []
@@ -30,30 +53,36 @@ def Read(filename,strip_whitespace=True,row_separator='\n',col_separator=','):
     
     data = numpy.array(data,dtype=datatypes)
     
-    return (title,data)
+    return {"title":title,"data":data}
 
-def drawViewFigure(viewer_fig,data_tuples,x_label,y_label):
+def drawViewFigure(viewer_fig,data_dicts,x_label,y_label):
+    """
+    Helper function for redrawing the view window for the x and y labels.
+    """
     viewer_fig.clear()
     viewer_ax = viewer_fig.add_subplot(111)
     
-    for dt in data_tuples:
-        title = dt[0]
-        data = dt[1]
-        viewer_ax.plot(data[x_label],data[y_label],label=title) #rather than replotting every time, maybe just change the data ranges?
+    for dd in data_dicts:
+        title = dd["title"]
+        data = dd["data"]
+        if(x_label in data.dtype.names and y_label in data.dtype.names): viewer_ax.plot(data[x_label],data[y_label],label=title) #rather than replotting every time, maybe just change the data ranges?
         
     viewer_fig.canvas.draw()
     
-def Plot(data_tuples):
-    
+def Plot(data_dicts):
+    """
+    High level function for plotting the data returned from the Read function
+    """
     viewer_fig = plt.figure()
     viewer_ax = viewer_fig.add_subplot(111)
     
     x_label = 0
     y_label = 1
     
+    #Finding the column names
     columns = []
-    for dt in data_tuples:
-        data = dt[1]
+    for dd in data_dicts:
+        data = dd["data"]
         for datatype_name in data.dtype.names:
             if(datatype_name not in columns): columns.append(datatype_name)
             
@@ -61,7 +90,7 @@ def Plot(data_tuples):
     y_label = columns[y_label]
     
     
-    drawViewFigure(viewer_fig,data_tuples,x_label,y_label)
+    drawViewFigure(viewer_fig,data_dicts,x_label,y_label)
      
     controls_fig = plt.figure()
     axcolor = 'w'
@@ -70,7 +99,7 @@ def Plot(data_tuples):
     
     def x_axesChange(label):
         x_label = label
-        drawViewFigure(viewer_fig,data_tuples,x_label,y_label)
+        drawViewFigure(viewer_fig,data_dicts,x_label,y_label)
         
     radio.on_clicked(x_axesChange)
     
@@ -79,19 +108,18 @@ def Plot(data_tuples):
     
     def y_axesChange(label):
         y_label = label
-        drawViewFigure(viewer_fig,data_tuples,x_label,y_label)
+        drawViewFigure(viewer_fig,data_dicts,x_label,y_label)
         
     radio2.on_clicked(y_axesChange)
     
-    #plt.legend(loc="best")
     plt.show()
 
 if __name__ == '__main__':
     if(len(sys.argv)>1):
-        data_tuples = []
-        for filename in sys.argv[1:]: data_tuples.append(Read(filename))
+        data_dicts = []
+        for filename in sys.argv[1:]: data_dicts.append(Read(filename))
         
-        Plot(data_tuples)
+        Plot(data_dicts)
     
     else:
         print "usage: BlackBoxDataVisualiser [data file 1] [data file 2] ... [data file n]"
